fc79d87582b427928682049f54d27246a5e17fcc
ssm0318 <kleenex0318@gmail.com>
2019-01-10 17:48:19 +0900

    water

---
+++ background.js
@@ -0,0 +1,24 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+'use strict';
+
+chrome.alarms.onAlarm.addListener(function() {
+  chrome.browserAction.setBadgeText({text: ''});
+  chrome.notifications.create({
+      type:     'basic',
+      iconUrl:  'icon.png',
+      title:    '어도어(adoor)',
+      message:  '새로운 노티가 도착했습니다.',
+      buttons: [
+        {title: 'Check Notifications'}
+      ],
+      priority: 0});
+});
+
+chrome.notifications.onButtonClicked.addListener(function() {
+  chrome.storage.sync.get(['minutes'], function(item) {
+    chrome.browserAction.setBadgeText({text: 'ON'});
+    chrome.alarms.create({delayInMinutes: item.minutes});
+  });
+});
